ab1e399c6dfab77b9dd91a0bd1e5f5a2d1290c8b
Pavel <troshpaw@gmail.com>
2023-10-11 10:30:48 +0300

    add class Student

--- test.php
+++ test.php
@@ -1,3 +1,23 @@
 
 <?php
+    class Student {
+        public function __construct($name) {
+            $this->name = $name;
+        }
+
+        public function print() {
+            echo "Name: " . $this->name;
+        }
+    }
+
+    $alex = new Student("Alex");
+    
+    print_r($alex);
+    echo "<br>";
+
+    $alex->print();
+    echo "<br>";
+
+    echo $alex->name;
+    echo "<br>";
 ?>
